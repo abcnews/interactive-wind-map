1fd9ecfbec85f442441749b2f430f5fd0e905e94
Colin Gourlay <gourlay.colin@abc.net.au>
2019-11-13 15:55:13 +1000

    Smaller arrows on smaller devices.

--- src/components/Arrow/index.js
+++ src/components/Arrow/index.js
@@ -1,9 +1,11 @@
 import { animSpeedFromWindSpeed, colorFromWindSpeed, tailHeightFromWindSpeed } from '../../utils';
 import styles from './styles.scss';
 
-const HEAD_WIDTH = 9;
-const HEAD_HEIGHT = 8;
-const TAIL_WIDTH = 2;
+const SHOULD_USE_SMALLER_ARROWS = window.innerWidth <= 640;
+
+const HEAD_WIDTH = SHOULD_USE_SMALLER_ARROWS ? 7 : 9;
+const HEAD_HEIGHT = SHOULD_USE_SMALLER_ARROWS ? 6 : 8;
+const TAIL_WIDTH = SHOULD_USE_SMALLER_ARROWS ? 1 : 2;
 const DIR_TO_DEG = {
   N: 0,
   NNE: 22.5,
@@ -29,7 +31,7 @@ export default function Arrow({ speed, dir } = {}) {
 
   const animSpeed = animSpeedFromWindSpeed(speed);
   const color = colorFromWindSpeed(speed);
-  const tailHeight = tailHeightFromWindSpeed(speed);
+  const tailHeight = tailHeightFromWindSpeed(speed) * (SHOULD_USE_SMALLER_ARROWS ? 0.75 : 1);
   const height = HEAD_HEIGHT + tailHeight;
   const path = `M${HEAD_WIDTH / 2} ${height}L0 ${tailHeight}h${(HEAD_WIDTH - TAIL_WIDTH) /
     2}V0h${TAIL_WIDTH}v${tailHeight}H${HEAD_WIDTH}l-${HEAD_WIDTH / 2} ${HEAD_HEIGHT}z`;
